03eebcf5ed66ca43c0220b1c9e86961ba5776614
akshay58165 <typeakshay@gmail.com>
2017-10-31 21:24:22 +0530

    adding settings configuration for psql

--- atoz/atoz/settings.py
+++ atoz/atoz/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'atoz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'atoz_db',
+        'USER': 'akshay',
+        'PASSWORD': 'jusluv2leev'
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
